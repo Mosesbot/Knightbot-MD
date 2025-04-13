381b866c47f8d198f92f3f4250ca6155ef0fe07b
Mosesbot <mosesakankpale@gmail.com>
2025-04-13 20:23:08 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Moses Akankpale', // Your name
-  ownerNumber: '0598580861', //Your number
+  ownerNumber: '0537200126', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
